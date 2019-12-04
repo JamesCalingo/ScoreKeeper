41b37da8cb107bb75f38274845f2ab67c9a64bbd
JC <james.calingo@gmail.com>
2019-12-04 08:49:18 -0500

    html update

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,13 @@
-alert("hi!")
+let p1=0
+let p2=0
+let p3=0
+let p4=0
+let p5=0
+let p6=0
+
+$("#p1Score").html(p1)
+$("#p2Score").html(p2)
+$("#p3Score").html(p3)
+$("#p4Score").html(p4)
+$("#p5Score").html(p5)
+$("#p6Score").html(p6)
