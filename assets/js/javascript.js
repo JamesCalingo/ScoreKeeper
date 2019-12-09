667d2194748009e0fd508859dc5a37dce28fc6ff
JC <james.calingo@gmail.com>
2019-12-09 10:21:39 -0500

    creating a beta folder

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,14 +1,22 @@
-let p1=0;
-let p2=0;
+let p1={
+  name: "Player 1",
+  score: 0
+};
+let p2={
+  name: "Player 2",
+  score: 0
+};
 let p3=0;
 let p4=0;
 let p5=0;
 let p6=0;
 let p7=0;
-let p8=0
+let p8=0;
 
-$("#p1Score").html(p1);
-$("#p2Score").html(p2);
+$(".name1").html(p1.name)
+
+$("#p1Score").html(p1.score);
+$("#p2Score").html(p2.score);
 $("#p3Score").html(p3);
 $("#p4Score").html(p4);
 $("#p5Score").html(p5);
@@ -17,8 +25,8 @@ $("#p7Score").html(p7);
 $("#p8Score").html(p8);
 
 function updateScores(){
-  $("#p1Score").html(p1);
-  $("#p2Score").html(p2);
+  $("#p1Score").html(p1.score);
+  $("#p2Score").html(p2.score);
   $("#p3Score").html(p3);
   $("#p4Score").html(p4);
   $("#p5Score").html(p5);
@@ -30,25 +38,25 @@ function updateScores(){
   
   $("#p1ScoreUp").on("click", function (event){
     event.preventDefault();
-    p1++;
+    p1.score++;
     updateScores();
   });
   
   $("#p1ScoreDn").on("click", function (event){
     event.preventDefault();
-    p1--;
+    p1.score--;
     updateScores()
   });
   
   $("#p2ScoreUp").on("click", function (event){
     event.preventDefault();
-    p2++;
+    p2.score++;
     updateScores()
   });
   
   $("#p2ScoreDn").on("click", function (event){
     event.preventDefault();
-    p2--;
+    p2.score--;
     updateScores()
   });
   
