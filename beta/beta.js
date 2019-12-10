8ef736c828835b35ae3d3e55c73da863aaf78ed9
JC <james.calingo@gmail.com>
2019-12-10 14:13:35 -0500

    trying to foolproof naming function

--- beta/beta.js
+++ beta/beta.js
@@ -13,13 +13,22 @@ $(".name2").html(p2.name);
 $("#p2Score").html(p2.score)
 }
 
-$(".btn-primary").on("click", function(e){
+$(".nameChange").on("click", function(e){
   e.preventDefault();
+  
   $(".name1").html(`<input class="form-control form-control-sm" id="newName" type="text" placeholder=""><button id="confirmChange" class="btn btn-sm btn-block">Confirm</button>`);
+  $(".btn-primary").addClass("disabled").removeClass("nameChange")
   $(document).on("click", "#confirmChange", function(e){
     e.preventDefault();
     let newName = $("#newName").val().trim();
-    $(".name1").html(newName)
+    if (!newName){
+      alert("What's your name?")
+      return false
+    }
+    else{
+    $(".name1").html(newName);
+    $(".btn-primary").removeClass("disabled").addClass("nameChange")
+    }
   })
 })
 
