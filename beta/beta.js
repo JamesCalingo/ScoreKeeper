2cbe468f354a79c632c63938515261ab20f267ca
JC <james.calingo@gmail.com>
2019-12-11 10:36:21 -0500

    fixed one problem

--- beta/beta.js
+++ beta/beta.js
@@ -18,18 +18,20 @@ $(".nameChange").on("click", function(e){
   
   $(".name1").html(`<input class="form-control form-control-sm" id="newName" type="text" placeholder=""><button id="confirmChange" class="btn btn-sm btn-block">Confirm</button>`);
   $(".btn-primary").addClass("disabled").removeClass("nameChange")
-  $(document).on("click", "#confirmChange", function(e){
-    e.preventDefault();
-    let newName = $("#newName").val().trim();
-    if (!newName){
-      alert("What's your name?")
-      return false
-    }
-    else{
-    $(".name1").html(newName);
-    $(".btn-primary").removeClass("disabled").addClass("nameChange")
-    }
-  })
+  
+})
+
+$(document).on("click", "#confirmChange", function(e){
+  e.preventDefault();
+  let newName = $("#newName").val().trim();
+  if (!newName){
+    alert("What's your name?")
+    return false
+  }
+  else{
+  $(".name1").html(newName);
+  $(".btn-primary").removeClass("disabled").addClass("nameChange")
+  }
 })
 
 showData()
