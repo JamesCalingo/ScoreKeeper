0788f3952c48f37d03abbe715f55480a02b25388
JC <james.calingo@gmail.com>
2019-12-12 11:26:50 -0500

    updated 8 player page

--- assets/js/names.js
+++ assets/js/names.js
@@ -1,8 +1,20 @@
 let player1 = "Player 1";
 let player2 = "Player 2";
+let player3 = "Player 3";
+let player4 = "Player 4";
+let player5 = "Player 5";
+let player6 = "Player 6";
+let player7 = "Player 7";
+let player8 = "Player 8";
 
 $(".name1").html(player1)
 $(".name2").html(player2)
+$(".name3").html(player3)
+$(".name4").html(player4)
+$(".name5").html(player5)
+$(".name6").html(player6)
+$(".name7").html(player7)
+$(".name8").html(player8)
 
 $(".btn-primary").on("click", function(){
   alert("You clicked the name change button!")
