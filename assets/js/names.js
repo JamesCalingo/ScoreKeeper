236bbcd169df7c638971ee89167252302f42cb8a
JC <james.calingo@gmail.com>
2019-12-12 11:01:50 -0500

    changing things with buttons

--- assets/js/names.js
+++ assets/js/names.js
@@ -3,3 +3,7 @@ let player2 = "Player 2";
 
 $(".name1").html(player1)
 $(".name2").html(player2)
+
+$(".btn-primary").on("click", function(){
+  alert("You clicked the name change button!")
+})
--- beta/beta.js
+++ beta/beta.js
@@ -13,15 +13,15 @@ $(".name2").html(p2.name);
 $("#p2Score").html(p2.score)
 }
 
-$(".nameChange").on("click", function(e){
+$("#nameChange1").on("click", function(e){
   e.preventDefault();
   
-  $(".name1").html(`<input class="form-control form-control-sm" id="newName" type="text" placeholder=""><button id="confirmChange" class="btn btn-sm btn-block">Confirm</button>`);
-  $(".btn-primary").addClass("disabled").removeClass("nameChange")
+  $(".name1").html(`<input class="form-control form-control-sm" id="newName" type="text" placeholder=""><button id="confirmChange1" class="btn btn-sm btn-block">Confirm</button>`);
+  $("#nameChange1").addClass("disabled").removeClass("nameChange")
   
 })
 
-$(document).on("click", "#confirmChange", function(e){
+$(document).on("click", "#confirmChange1", function(e){
   e.preventDefault();
   let newName = $("#newName").val().trim();
   if (!newName){
@@ -30,6 +30,27 @@ $(document).on("click", "#confirmChange", function(e){
   }
   else{
   $(".name1").html(newName);
+  $(".btn-primary").removeClass("disabled").addClass("nameChange")
+  }
+})
+
+$("#nameChange2").on("click", function(e){
+  e.preventDefault();
+  
+  $(".name2").html(`<input class="form-control form-control-sm" id="newName" type="text" placeholder=""><button id="confirmChange2" class="btn btn-sm btn-block">Confirm</button>`);
+  $("#nameChange2").addClass("disabled").removeClass("nameChange")
+  
+})
+
+$(document).on("click", "#confirmChange2", function(e){
+  e.preventDefault();
+  let newName = $("#newName").val().trim();
+  if (!newName){
+    alert("What's your name?")
+    return false
+  }
+  else{
+  $(".name2").html(newName);
   $(".btn-primary").removeClass("disabled").addClass("nameChange")
   }
 })
